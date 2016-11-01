611d6fb99f53f48cea2f971f76d02b0cb0d8e6b8
Andreas Hoerster <andreas-hoerster@web.de>
2016-11-01 15:11:34 +0100

    Initial commit

---
+++ src/main/java/UPN/InfixToUPN.java
@@ -0,0 +1,18 @@
+package UPN;
+
+import java.util.Queue;
+
+/**
+ * Created by andi on 01.11.16.
+ */
+public final class InfixToUPN {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Queue<String> process(String line) {
+
+    }
+
+}
---
+++ src/main/java/UPN/OpUtil.java
@@ -0,0 +1,16 @@
+package UPN;
+
+/**
+ * Created by andi on 01.11.16.
+ */
+public final class OpUtil {
+
+    public static boolean lowerPrecedence(String op1, String op2) {
+
+    }
+
+    public static boolean higherPrecedence(String op1, String op2) {
+
+    }
+
+}
