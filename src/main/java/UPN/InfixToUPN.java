ab47f49090005c5483779b7a0965fa333c297e2c
trashtatur <andreas-hoerster@web.de>
2016-11-02 18:35:17 +0100

    Added the process method in InfixToUPN... damn that one goes deep

--- src/main/java/UPN/InfixToUPN.java
+++ src/main/java/UPN/InfixToUPN.java
@@ -1,7 +1,8 @@
 package UPN;
 
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -14,9 +15,62 @@ public final class InfixToUPN {
     }
 
     public static Queue<String> process(String line) {
+
+        //1. Lege eine Queue und einen Stack an.
+        Stack<String> stack=new Stack();
+        Queue<String> queue=new LinkedBlockingQueue<String>();
+
         Scanner scanner=new Scanner(line);
         Pattern delim=Pattern.compile("\\s");
         scanner.useDelimiter(delim);
+
+        Pattern numbers=Pattern.compile("\\d");
+        Matcher matchNum=numbers.matcher(scanner.next());
+
+        Pattern operators=Pattern.compile("\\+|-|\\*|/|\\(|\\)|,");
+        Matcher matchOp=operators.matcher(scanner.next());
+
+        //2. Lies die Zeichenkette von links nach rechts
+        while (scanner.hasNext()) {
+            //3. Wird eine Zahl gelesen wird, dann schreibe diese in die Queue
+            if (matchNum.matches()) {
+                queue.add(scanner.next());
+            }
+            //4. Wird ein Operator (Variable op) gelesen, dann
+            else if (matchOp.matches()) {
+                String op=scanner.next();
+                /* 4 a) a. wenn der Stack leer ist oder das oberster Element des Stacks die öffnende
+                Klammer ist ('(') oder das oberste Element des Stacks geringere Präzedenz als op
+                hat, dann lege op auf den Stack. Präzedenzregel:
+                '(' vor '/' vor '*' vor '-' vor '+'vor ')'
+                */
+                if (stack.empty() ||
+                    stack.peek().equals("(") ||
+                    OpUtil.lowerPrecedence(stack.peek(),op)) {
+
+                        stack.push(op);
+
+                }
+                /* 4 b) sonst: nehme solange Operatoren vom Stack und schreibe diese in die Queue, wie
+                        die Operatoren auf dem Stack höhere Präzedenz als op haben. Öffnende
+                        Klammern '(' werden vom Stack genommen und nicht in die Queue geschrieben.
+                        Danach wird op auf den Stack gelegt. Ausnahme: Schließende Klammern werden
+                        nicht auf den Stack gelegt.
+                */
+                else {
+                    while (OpUtil.higherPrecedence(stack.peek(),op)) {
+                        // .... down down the rabbit hole! Always deeper down!
+                        if (stack.peek().equals("(")) {
+                            stack.pop();
+                        }
+                        else {
+                            queue.add(stack.pop());
+                        }
+                    }
+                }
+            }
+        }
+    return queue;
     }
 
 }
