7268d289dbc84681421242d88c3c50deb8fd05ce
trashtatur <andreas-hoerster@web.de>
2016-11-03 22:40:50 +0100

    Added the base for the scanner. Rest is still to do

--- src/main/java/InfixCalc/UPNTaschenrechner.java
+++ src/main/java/InfixCalc/UPNTaschenrechner.java
@@ -1,5 +1,10 @@
 package InfixCalc;
 
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Andi on 03.11.2016.
  */
@@ -9,6 +14,18 @@ public final class UPNTaschenrechner {
 
     }
     public static Double process(String line) {
+        Stack<Double> stack= new Stack<Double>();
+        Scanner scanner=new Scanner(line);
+        Pattern delim=Pattern.compile("\\s");
+        scanner.useDelimiter(delim);
 
+        Pattern numbers=Pattern.compile("\\d+");
+        Pattern operators=Pattern.compile("\\+|–|\\*|/|\\(|\\)|,");
+
+        while (scanner.hasNext()) {
+            String actualToken=scanner.next();
+            Matcher matchNum=numbers.matcher(actualToken);
+            Matcher matchOp=operators.matcher(actualToken);
+        }
     }
 }
