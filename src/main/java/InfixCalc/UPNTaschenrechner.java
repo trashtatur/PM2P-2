77d2cc69817e9a8dec800a6236baf805510edeb5
trashtatur <andreas-hoerster@web.de>
2016-11-03 22:25:49 +0100

    Initial commit with method frames

---
+++ src/main/java/InfixCalc/UPNTaschenrechner.java
@@ -0,0 +1,14 @@
+package InfixCalc;
+
+/**
+ * Created by Andi on 03.11.2016.
+ */
+public final class UPNTaschenrechner {
+
+    public static void main(String[] args) {
+
+    }
+    public static Double process(String line) {
+
+    }
+}
